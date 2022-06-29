fb5fa29d354c69571e1f0f2891c9dc21a87b1cdc
Sathish <82315097+rsk1986@users.noreply.github.com>
2022-06-29 21:10:30 +0530

    Update zip-add-folder.sh

--- zip-add-folder.sh
+++ zip-add-folder.sh
@@ -1,15 +1,15 @@
 https://www.geeksforgeeks.org/zip-command-in-linux-with-examples/
 
 
-[iceadminPD@sesklentsspd03 fileintegrity]$ pwd
+[iceadminPD@server1 fileintegrity]$ pwd
 /entimice/fileintegrity
 
-[iceadminPD@sesklentsspd03 fileintegrity]$ ls -lrt
+[iceadminPD@server1 fileintegrity]$ ls -lrt
 total 2612
 -rw-r--r--.  1 iceadminPD icegroupPD 2657418 Jan  8 14:58 fileintegrity.zip.old
 drwxr-xr-x. 11 iceadminPD icegroupPD    4096 Jan  9 15:36 fileintegrity
 
-[iceadminPD@sesklentsspd03 fileintegrity]$ zip -r fileintegrity.zip fileintegrity/
+[iceadminPD@server1 fileintegrity]$ zip -r fileintegrity.zip fileintegrity/
   adding: fileintegrity/ (stored 0%)
   adding: fileintegrity/FilesIntegrityChecker_PRD_20July2020/ (stored 0%)
   adding: fileintegrity/FilesIntegrityChecker_PRD_20July2020/ConfigFile.xml (deflated 54%)
@@ -30,10 +30,10 @@ zip warning: Permission denied
 
 zip warning: Not all files were readable
   files/entries read:  20286 (1.7G bytes)  skipped:  4 (2.7K bytes)
-[iceadminPD@sesklentsspd03 fileintegrity]$ cat fileintegrity/FilesIntegrityChecker_PRD_9Jan2021_cdar/nohup.out
+[iceadminPD@server1 fileintegrity]$ cat fileintegrity/FilesIntegrityChecker_PRD_9Jan2021_cdar/nohup.out
 cat: fileintegrity/FilesIntegrityChecker_PRD_9Jan2021_cdar/nohup.out: Permission denied
 
-[iceadminPD@sesklentsspd03 fileintegrity]$ ls -lrth
+[iceadminPD@server1 fileintegrity]$ ls -lrth
 total 239M
 -rw-r--r--.  1 iceadminPD icegroupPD 2.6M Jan  8 14:58 fileintegrity.zip.old
 drwxr-xr-x. 11 iceadminPD icegroupPD 4.0K Jan  9 15:36 fileintegrity
