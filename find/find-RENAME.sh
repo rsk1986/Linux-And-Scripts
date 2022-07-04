ccc00b767bfa540f1868b081231a3e88d632e8ba
Sathish <82315097+rsk1986@users.noreply.github.com>
2022-07-04 21:12:32 +0530

    Update find-RENAME.sh

--- find/find-RENAME.sh
+++ find/find-RENAME.sh
@@ -1,4 +1,4 @@
-[iceadminPD@sesklentfspd01 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec ls -l {} \;
+[admin@server1 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec ls -l {} \;
 -rw-------. 1 iceadminPD icegroupPD 26103 Oct 28 12:12 /entimice/filesystem/temp/tmp_cmd_testdata_CR_Change_Autoversioning_Tune.log
 -rw-------. 1 iceadminPD icegroupPD 29147 Oct 28 12:13 /entimice/filesystem/temp/tmp_cmd_testdata_CR200_Tune.log
 -rw-------. 1 iceadminPD icegroupPD 31445 Oct 28 12:16 /entimice/filesystem/temp/tmp_cmd_testdata_CR201_Tune.log
@@ -11,12 +11,12 @@
 -rw-------. 1 iceadminPD icegroupPD 39013 Oct 28 12:27 /entimice/filesystem/temp/tmp_cmd_testdata_CR208_Tune.log
 -rw-------. 1 iceadminPD icegroupPD 30428 Nov  2 21:02 /entimice/filesystem/temp/tmp_cmd_testdata_CR151_Tune.log
 -rw-------. 1 iceadminPD icegroupPD 27382 Nov  2 21:03 /entimice/filesystem/temp/tmp_cmd_testdata_CR209_Tune.log
-[iceadminPD@sesklentfspd01 ~]$
+[admin@server1 ~]$
 
-[iceadminPD@sesklentfspd01 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec mv {} {}.06Nov2020 \;
-[iceadminPD@sesklentfspd01 ~]$
+[admin@server1 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec mv {} {}.06Nov2020 \;
+[admin@server1 ~]$
 
-[iceadminPD@sesklentfspd01 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec ls -l {} \;
+[admin@server1 ~]$ find /entimice/filesystem/temp -type f -name 'tmp_cmd_testdata_CR*' -exec ls -l {} \;
 -rw-------. 1 iceadminPD icegroupPD 29147 Oct 28 12:13 /entimice/filesystem/temp/tmp_cmd_testdata_CR200_Tune.log.06Nov2020
 -rw-------. 1 iceadminPD icegroupPD 31445 Oct 28 12:16 /entimice/filesystem/temp/tmp_cmd_testdata_CR201_Tune.log.06Nov2020
 -rw-------. 1 iceadminPD icegroupPD 26103 Oct 28 12:12 /entimice/filesystem/temp/tmp_cmd_testdata_CR_Change_Autoversioning_Tune.log.06Nov2020
